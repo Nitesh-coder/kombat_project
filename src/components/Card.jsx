ac356a8d5863fbcb64c2ad1c683beeac100a1040
Nitesh Ghimire <initesh100@gmail.com>
2024-08-06 15:12:20 +0545

    Click function added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import bg from '../assets/bg.jpg'
 import goldImg from '../assets/gold.jpeg'
 
 export const Card = ({name, invest}) => {
+  const [num ,setNum] = useState(0)
+  function ClickMe(){
+    setNum(prev=>prev+1)
+    alert(`Click ${num} times`)
+  }
   return (
-    <div className=' md:w-36 md:h-48 w-48 h-60 border border-black rounded-md flex flex-col relative items-center justify-center' >
+    <div onClick={ClickMe} className=' md:w-36 md:h-48 w-48 h-60 border border-black rounded-md flex flex-col relative items-center justify-center' >
         <img src={bg} alt="bg" className=' w-full h-full object-fill -z-10 absolute' />
         <img src={goldImg} alt="goldImg" className=' w-32 h-30' />
         <h2 className=' font-bold text-md'>{name}</h2>
