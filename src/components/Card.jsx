a927f1de5c9fd0eeb00417310fa7d747f1ad2165
Nitesh Ghimire <initesh100@gmail.com>
2024-08-08 16:10:55 +0545

    pupup button added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
 import bg from '../assets/bg.jpg'
 import goldImg from '../assets/gold.jpeg'
+import { PopUp } from './PopUp'
 
 export const Card = ({name, invest}) => {
-  const [num ,setNum] = useState(0)
+  const [inputBox, setInputBox] = useState(false)
   function ClickMe(){
-    setNum(prev=>prev+1)
-    alert(`Click ${num} times`)
+    setInputBox(!inputBox)
   }
   return (
     <div onClick={ClickMe} className=' md:w-36 md:h-48 w-48 h-60 border border-black rounded-md flex flex-col relative items-center justify-center' >
@@ -14,6 +14,8 @@ export const Card = ({name, invest}) => {
         <img src={goldImg} alt="goldImg" className=' w-32 h-30' />
         <h2 className=' font-bold text-md'>{name}</h2>
         <h3>{invest}S</h3>
+
+        {inputBox && <PopUp />}
     </div>
   )
 }
