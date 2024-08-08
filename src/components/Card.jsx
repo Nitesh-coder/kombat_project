08a5124faf8cf1c8dd5c18300171f84490afb292
Nitesh Ghimire <initesh100@gmail.com>
2024-08-08 16:40:38 +0545

    add popupbtn

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,14 +8,19 @@ export const Card = ({name, invest}) => {
   function ClickMe(){
     setInputBox(!inputBox)
   }
+  function handleChildDiv(e){
+    e.stopPropagation()
+  }
   return (
-    <div onClick={ClickMe} className=' md:w-36 md:h-48 w-48 h-60 border border-black rounded-md flex flex-col relative items-center justify-center' >
+    <div onClick={ClickMe} className=' select-none md:w-36 md:h-48 w-48 h-60 border border-black rounded-md flex flex-col relative items-center justify-center' >
         <img src={bg} alt="bg" className=' w-full h-full object-fill -z-10 absolute' />
         <img src={goldImg} alt="goldImg" className=' w-32 h-30' />
         <h2 className=' font-bold text-md'>{name}</h2>
         <h3>{invest}S</h3>
 
-        {inputBox && <PopUp />}
+        <div onClick={handleChildDiv} className=' absolute'>
+          {inputBox && <PopUp />}
+        </div>
     </div>
   )
 }
--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export const PopUp = () => {
   return (
-    <div>PopUp</div>
+    <div className='shadow-md flex items-center justify-center flex-col'>
+        <input className=' h-10 w-24 outline-none text-center' type="text" />
+        <button className=' h-7 w-16 bg-orange-600'>DONE</button>
+    </div>
   )
 }
