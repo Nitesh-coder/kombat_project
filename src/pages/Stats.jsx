f7efeb833d3465a854d7e26fbeea34111f77c98a
Nitesh Ghimire <initesh100@gmail.com>
2024-08-19 17:10:31 +0545

    fix chart

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -2,34 +2,54 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios';
 import { Pie } from 'react-chartjs-2'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const Stats = () => {
     const [data, setData] = useState([])
+    const [name, setName] = useState([])
+
+    const [chartData, setChartData] = useState({
+      labels: [],
+      datasets: [
+        {
+          data: [],
+          backgroundColor: ['yellow', '#36A2EB', '#FFCE56', 'green'],
+          hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+        },
+      ],
+    })
 
     useEffect(()=>{
       const fetchData = async()=>{
         try {
           const response = await axios.get("https://kombat-backend.vercel.app/api")
-          setData(response.data)
+          const datas = response.data.map(item => item.invest)
+          const names = response.data.map(item=>item.name)
+          setData(datas)
+          setName(names)
         } catch (error) {
           console.log(error);
         }
       }
       fetchData()
-      
     },[])
+    console.log(name);
 
-    const chartData = {
-        labels: [data[0].name, data[1].name, data[2].name],
-        datasets: [
-          {
-            data: [data[0].invest, data[1].invest, data[2].invest],
-            backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
-            hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
-          },
-        ],
-      };
+    useEffect(() => {
+      if (name.length > 0) {
+        setChartData((prevChartData) => ({
+          ...prevChartData,
+          labels: name,
+          datasets: [
+            {
+              data: data
+            }
+          ]
+        }));
+      }
+    }, [name]);
+
   return (
     <div className=' flex justify-center w-1/2'>
         <Pie data={chartData} />
